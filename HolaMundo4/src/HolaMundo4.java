eb884d1daacf3474f7d59c7cac612340e21eb9d0
sergioosma09 <sergioosma09@gmail.com>
2018-09-20 10:18:56 +0200

    Añado HolaMundo

--- HolaMundo4/src/HolaMundo4.java
+++ HolaMundo4/src/HolaMundo4.java
@@ -1,23 +1,28 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 public class HolaMundo4 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		String nombre;
 		Scanner tcl = new Scanner(System.in);
 		System.out.println("Introduce un nombre: ");
 		nombre=tcl.nextLine();
-		tcl.close();
-		
-		System.out.println("Crear fichero ficheroHolaMundo.txt");
-		String nomFichero="ficheroHolaMundo.txt";
-		BufferedWriter br = new BufferedWriter(new FileWriter(nomFichero));
-		
-		for(int i=0;i<20;i++)
+		for(int i=0;i<20;i++) 
 			System.out.println("Hola " + nombre);
-	}
+		
+		
+		try {
+		BufferedWriter br = new BufferedWriter(new FileWriter(new File("nomFichero.txt")));
+		
+		for(int i=0;i<20;i++) {
+			br.write("Hola " + nombre);
+			br.newLine();
+			}
+		br.close();
+		
+		}catch(Exception e) {
+			System.out.println("Error");
+		}
 
+}
 }
