63847c4378302b08b35e70cf6736af4012fcc1ac
sergioosma09 <sergioosma09@gmail.com>
2018-10-04 12:10:53 +0200

    Añadid el EjemploProccesBuilder2

---
+++ EjemploProccesBuilder2/src/RunTime2.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RunTime2 {
+
+	public static void main(String[] args) {
+
+		if (args.length <= 0) {
+			System.err.println("Se necesita un programa a ejecutar");
+			System.exit(-1);
+		}
+		Runtime runtime = Runtime.getRuntime();
+        		
+		try {
+			
+			Process process = runtime.exec(args, null, new File("/home/sersai/psp/EjemploRunTime/bin/"));
+			Scanner tcl=new Scanner(System.in);
+			System.out.println("Soy EjemploRunTime2.........");		
+			tcl.nextLine();
+			process.destroy();
+		} catch (IOException ex) {
+			System.err.println("Excepción de E/S");
+			System.exit(-1);
+		}
+	}
+}
