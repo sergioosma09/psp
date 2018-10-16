6b15abb286ef0ef005ae7b9d40423e6e7ff784ef
sergioosma09 <sergioosma09@gmail.com>
2018-10-16 08:17:50 +0200

    Añadido EjemploStrea1 y HolaMundo1

---
+++ EjemploStream1/src/EjemploStream1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class EjemploStream1 {
+
+	public static void main(String args[]) throws IOException {
+		ProcessBuilder process = new ProcessBuilder();
+		process.directory(new File( "/home/sersai/psp/EjemploStream1/bin/"));
+		Process proces = new ProcessBuilder(args).start();
+		InputStream is = proces.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		System.out.println("Salida del proceso " + Arrays.toString(args) + " :");
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+
+}
