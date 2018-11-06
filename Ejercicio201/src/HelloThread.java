db2a9042de79577451daf8655435dc8276eb15a5
sergioosma09 <sergioosma09@gmail.com>
2018-11-06 10:42:34 +0100

    Añadido ejercicio 201 y 202

---
+++ Ejercicio201/src/HelloThread.java
@@ -0,0 +1,7 @@
+
+
+class HelloThread extends Thread {
+	public void run() {
+		System.out.println("Hola desde el hilo creado!");
+	}
+}
---
+++ Ejercicio201/src/RunThread.java
@@ -0,0 +1,10 @@
+
+public class RunThread {
+
+	public static void main(String args[]) {
+		HelloThread hilo = new HelloThread(); // Se crea un nuevo hilo de ejecución
+		hilo.start(); // se arranca el hilo creado anteriormente
+		System.out.println("Hola desde el hilo principal!");
+		System.out.println("Proceso acabando");
+	}
+}
